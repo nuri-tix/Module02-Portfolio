6d4dbeb8a2f80b6c5c38b0dbfe28cab4a4be45a8
nuri-tix <artweb1509@gmail.com>
2022-05-16 17:11:11 +0300

    Add files

--- src/index.js
+++ src/index.js
@@ -5,35 +5,95 @@ import './index.html';
 
 
 
-// меню-----------------------------------------
+// popup 1ый вариант-----------------------------------------
+
+// const blackout = document.querySelector('.blackout');
+// const openPopup = document.querySelectorAll('.open-popup');
+// const closePopup = document.querySelectorAll('.popup-close');
+// const body = document.body;
+
+
+
+// function openModal(name) {
+//    document.body.className = 'modal-' + name;
+//    body.classList.add('locked');
+//    blackout.style.display = 'block';
+   
+// }
+
+// openPopup.forEach(el => {
+//    let name = el.getAttribute('data-name')
+//    el.addEventListener('click', () => { openModal(name) })
+//    console.log('>>', el)
+// });
+
+// function closeModal() {
+//    document.body.className = '';
+//    body.classList.remove('locked');
+//    blackout.style.display = 'none';
+// };
+
+// closePopup.forEach(el => {
+//    el.addEventListener('click', () => { closeModal() });
+//    console.log('>>', el)
+   
+// });
+
+// blackout.onclick = function (event) {
+//    if (event.target == blackout) {
+//       blackout.style.display = 'none';
+//       body.classList.remove('locked');
+//    }
+// };
+
+
+
+// popup 2ой вариант
+
+const openPopup = document.querySelectorAll('.open-popup');
+const popup = document.querySelectorAll('.popup');
+
 const body = document.body;
 
+function openModal(elem) {
+   elem.classList.add('is-active');
+   body.classList.add('locked');
+}
 
-const blackout = document.querySelector('.blackout');
-const btn = document.querySelector('.discuss');
-const closePopup = document.querySelector('.popup-close');
-console.log(btn);
-
-   function openModal(name) {
-      document.body.className = 'modal-' + name; 
-      blackout.style.display = 'block';
-   };
-   
-   
-   function closeModal() {
-      document.body.className = '';
-      blackout.style.display = 'none';
-   };
-
-btn.onclick = openModal;
-closePopup.onclick = closeModal;
-
-
-blackout.onclick = function (event) {
-   if (event.target == blackout) {
-      blackout.style.display = 'none';
+function closeModal(e) {
+   if (e.target.classList.contains('popup-close') || e.target.classList.contains('blackout')) {
+      e.target.closest('.popup').classList.remove('is-active');
+      body.classList.remove('locked');
    }
-};
+   
+}
+
+openPopup.forEach(btn => {
+   btn.addEventListener('click', (e) => {
+      let data = e.target.dataset.name;
+
+      popup.forEach(modal => {
+         if (modal.dataset.modal == data) {
+            openModal(modal);
+         }
+      }) 
+      
+   })
+})
+
+popup.forEach(modal => {
+   modal.addEventListener('click', e => closeModal(e))
+});
+
+window.addEventListener('keydown', e => {
+   popup.forEach(modal => {
+      if (e.key === "Escape" && modal.classList.contains('is-active'))
+         modal.classList.remove('is-active');
+         body.classList.remove('locked');
+   })
+})
+     
+
 
 // select
 
@@ -66,3 +126,29 @@ let select = function() {
 
 select();
 
+// let select = function() {
+//    let select = document.querySelector('.select');
+//    let selectResult = select.querySelector('.select__result');
+
+  
+  
+//   selectResult.addEventListener('click', selectToggle) 
+
+//   document.body.addEventListener('click', selectChoose)
+
+//    function selectToggle(e) {
+//      e.stopPropagation();
+//      this.parentElement.classList.toggle('active');
+//    };
+
+//    function selectChoose(e) {
+//      let inside = e.target.closest('.select');
+//      if(inside){
+//        selectResult.textContent = e.target.innerText;       
+//      }
+//      select.classList.remove('active')
+//    };
+   
+// };
+
+// select();
